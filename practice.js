6eb6e54151879a3cf8579ece2b538c25490ec1fb
amartin58 <adee1001@outlook.com>
2021-07-20 15:17:49 -0700

    Make three objects

---
+++ practice.js
@@ -0,0 +1,31 @@
+
+const bathroom = {
+    name: "Upstairs Bathroom",
+    color: "baby blue",
+    floor: "tile",
+    sinks: 2,
+    cabinets: 1,
+    linenCloset: true
+  }
+  console.log(bathroom)
+
+  const refridgerator = {
+    brand: "Bosch",
+    material: "Stainless Steel",
+    contents: {
+        vegetabes: ["broccoli", "spinach", "califlower"],
+        fruits: ["strawberries", "blueberries", "grapes"],
+        meats: ["steak", "pork chops"],
+        poultry: "salmon"
+    }
+  }
+
+  console.log(refridgerator)
+  const truck = {
+    make: "Ford",
+    model: "F-150 Lariot", 
+    transmission: "automatic",
+    seating: 6, 
+    engine: "2.7L EcoBoost V6"
+  }
+  console.log(truck)
